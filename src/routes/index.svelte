a1e8d6e7f7ed8edb0a17d804a1b009f43b1e5bef
Alan B <918773+alanb2718@users.noreply.github.com>
2022-02-21 12:29:19 -0800

    add mustard jar, initial greeting

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Dijon Spreadsheet Query Builder</title>
+</svelte:head>
+
+<section>
+	<h1>
+		<div class="mustard">
+			<picture>
+				<img src="mustard-jar.jpg" alt="Photo of a jar of Dijon mustard" />
+			</picture>
+		</div>
+	</h1>
+
+	<h2>
+		Dijon Spreadsheet Query Builder
+	</h2>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	.mustard {
+
+	}
+
+	.mustard img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 7%;
+	}
+</style>
